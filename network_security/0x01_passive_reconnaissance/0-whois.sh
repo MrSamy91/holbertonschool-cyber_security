da26e03f49d4ffb284e6474a3a17cfe0405833ae
MrSamy91 <samy.ajouid@gmail.com>
2025-05-01 18:17:43 +0200

    add

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk '/^Registrant|^Admin|^Tech/ && /:/ {split($0,a,":"); gsub(/^[ \t]+/, "", a[2]); print a[1] "," a[2]}' > $1.csv
+whois "$1" | awk -F': ' '$0 ~ /^(Registrant|Admin|Tech)/ {print $1 "," $2}' > "$1.csv"
